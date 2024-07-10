7d3aa43bd04867d6bd264d8d14a6064a5db8bbfc
tallhee <100551379+tallhee@users.noreply.github.com>
2024-07-10 18:26:34 +0900

    Update 07-chatbot2.py
    professional clinical pathologist role

--- 07-chatbot2.py
+++ 07-chatbot2.py
@@ -1,30 +1,26 @@
 import streamlit as st
-import os
 from langchain_openai import ChatOpenAI
+import os
 
 # Streamlit UI 설정
 st.set_page_config(page_title="ChatOpenAI Demo", page_icon=":robot:")
 st.header("ChatOpenAI Demo")
 
-# 사이드바에 OpenAI API 키 입력 필드 생성
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-
 if openai_api_key:
     os.environ["OPENAI_API_KEY"] = openai_api_key
 else:
     st.info("Please add your OpenAI API key to continue.")
     st.stop()
-    
 
 # ChatOpenAI 모델 초기화
 chat = ChatOpenAI(temperature=0)
 
-
 # 세션 상태 초기화
 if "messages" not in st.session_state:
     st.session_state.messages = [
-        {"role": "system", "content": "You are a helpful assistant."}
+        {"role": "system", "content": "You are a professional clinical pathologist."}
     ]
 
 # 대화 히스토리 표시
